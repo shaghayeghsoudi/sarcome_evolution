e119f2f1b8e9d6588c597e64d62bd1cbbd2ac632
Shaghayegh Soudi <annasoudi@gmail.com>
2023-02-02 09:18:24 -0800

    new script added

---
+++ Gistic/run_GISTIC.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+## run example GISTIC analysis
+
+## output directory
+echo --- creating output directory ---
+basedir=`pwd`/example_results
+mkdir -p $basedir 
+
+echo --- running GISTIC ---
+## input file definitions
+#segfile=`pwd`/examplefiles/segmentationfile_9samples_NA.txt
+segfile=/home/hmei/FreeC.txt
+refgenefile=`pwd`/refgenefiles/hg38.UCSC.add_miR.160920.refgene.mat
+./gistic2 -b $basedir -seg $segfile -refgene $refgenefile
