0d287748eeca7bff228033da737bb4262a80baca
Shaghayegh Soudi <annasoudi@gmail.com>
2023-06-26 13:39:34 -0700

    header updated

--- mutational_signature/mutsignatures-analysis_merged_regions.R
+++ mutational_signature/mutsignatures-analysis_merged_regions.R
@@ -4,7 +4,7 @@
 # Original Author:  Shaghayegh Soudi
 # Contributors:    NA 
 # Date: November 2022
-# Updated: February 2023
+# Updated: June 2023
 ## for vignette see: # https://cran.r-project.org/web/packages/mutSignatures/vignettes/get_sarted_with_mutSignatures.html
 
 
