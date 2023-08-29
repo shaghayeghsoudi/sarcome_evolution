54bcff7056d72a23ad9f008160ea8cb43a0e7e1d
shaghayeghsoudi <annasoudi@gmail.com>
2023-08-29 15:17:09 -0700

    forest with ggplot code added

--- mutational_landscape/pretty_forest_plot_mut_differences.R
+++ mutational_landscape/pretty_forest_plot_mut_differences.R
@@ -6,10 +6,6 @@ library(metafor)
 library(dplyr)
 library(broom)  ## The broom package takes the messy output of built-in functions in R, such as lm, nls, or t.test, and turns them into tidy tibbles
 
-## useful links
-#https://stackoverflow.com/questions/52784396/fishers-exact-test-on-rows-in-data-frame-r
-#https://stackoverflow.com/questions/63528274/integrate-odds-ratio-and-ci-with-p-value-in-fisher-test-r
-#http://genometoolbox.blogspot.com/2014/06/easy-forest-plots-in-r.html
 
 cases<-read.csv(file="~/Dropbox/cancer_reserach/sarcoma/sarcoma_analysis/gistic/gistic_onesample_highest_purity_May2023/output_unmarged_ZackNormalised_singlesample_highest_purity_X_plot/costum_plots/sarcoma_mutated_cases.csv", header = TRUE)
 #head(cases)
@@ -26,7 +22,7 @@ mat_fisher<-apply(cases_good, 1, function(x) {
   tbl <- matrix(as.numeric(x[1:4]), ncol=2, byrow=T)   ### make 2*2 matirx for each row
   ft <- fisher.test(tbl, alternative="two.sided")      ### do fisher test for each matrix
   tidy(ft) %>%                                         ### tidy functions belongs to "groom" package to tide up fisher results
-    dplyr::select(estimate, p.value, conf.low, conf.high) }) %>% 
+    dplyr::select(estimate, p.value, conf.low, conf.high) }) %>%   ### estimate is the odds ratio value
     bind_rows(.id = 'grp')      
 
 mat_fisher_good<-data.frame(mat_fisher)
@@ -39,18 +35,18 @@ names(final_table_forsupp)[5]<-"odds_ratio"
 
 ### calulate adjusted p-value
 final_table_forsupp$Adj_pvalue<-p.adjust(final_table_forsupp$p.value, method = "BH")
-
-
 write.table(final_table_forsupp, file = "~/Dropbox/cancer_reserach/sarcoma/sarcoma_analysis/gistic/gistic_onesample_highest_purity_May2023/output_unmarged_ZackNormalised_singlesample_highest_purity_X_plot/costum_plots/diff_mutation_ratio_fisher_test_irradiated_vs_radiated.txt", row.names = TRUE, col.names = TRUE, sep = "\t", quote = FALSE)
 
-### plot forest plot
-qq_good$beta <- log(qq_good$estimate)
-qq_good$se   <- (log(qq_good$conf.high)-log(qq_good$conf.low))/(2*1.96)
+
+### make the forest plot 
+### reference from this website: http://genometoolbox.blogspot.com/2014/06/easy-forest-plots-in-r.html
+mat_fisher_good$beta <- log(mat_fisher_good$estimate)
+mat_fisher_good$se   <- (log(mat_fisher_good$conf.high)-log(mat_fisher_good$conf.low))/(2*1.96)
 
 # Assign values for plotting
-labs <- qq_good$grp
-yi   <- qq_good$beta
-sei  <- qq_good$se
+labs <- mat_fisher_good$grp
+yi   <- mat_fisher_good$beta
+sei  <- mat_fisher_good$se
 
 # Combine data into summary estimate
 res  <- rma(yi=yi, sei=sei, method="FE")
@@ -61,5 +57,18 @@ mtext(paste("Association p-value=",summary(res)$pval),side=3, line=-1)
 
 
 
+#### alternative, quick way of plotting with ggplot ()
+### reference link: https://rpubs.com/mbounthavong/forest_plots_r
 
+ggplot(mat_fisher_good, aes(y = grp, x = estimate)) +
+  geom_point(shape = 18, size = 5) +  
+  geom_errorbarh(aes(xmin = conf.low, xmax = conf.high), height = 0.25) +
+  geom_vline(xintercept = 1, color = "red", linetype = "dashed", cex = 1, alpha = 0.5) 
+  
+#############
+#### END ####
+#############
+## reference links
+#https://stackoverflow.com/questions/52784396/fishers-exact-test-on-rows-in-data-frame-r
+#https://stackoverflow.com/questions/63528274/integrate-odds-ratio-and-ci-with-p-value-in-fisher-test-r
 
