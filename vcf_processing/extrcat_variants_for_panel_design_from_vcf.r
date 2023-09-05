6d749da70b18bd64bea2d77b182d5a1b77a41966
shaghayeghsoudi <annasoudi@gmail.com>
2023-09-05 13:59:02 -0700

    extra lines removed

--- vcf_processing/extrcat_variants_for_panel_design_from_vcf.r
+++ vcf_processing/extrcat_variants_for_panel_design_from_vcf.r
@@ -1,23 +1,17 @@
 #rm(list = ls())
 
-### extrcat variants for the panel design (from vcf files genearted by survivor)###
+####################################################################################
+### extrcat variants for the panel design (from vcf files genearted by survivor) ###
+####################################################################################
 
-#https://krassowski.github.io/complex-upset/articles/Examples_R.html#fill-the-bars
-###################################
-##### analyze survivor consensus vcf files
-###################################
-library(ggplot2)
-library(webr)
+#library(webr)
 library(dplyr)
-library(plyr)
-library(vcfR)
-library(ggplot2)
-library(tidyverse)
-library(StructuralVariantAnnotation)
+#library(plyr)
+#library(vcfR)
+#library(tidyverse)
+#library(StructuralVariantAnnotation)
 library(VariantAnnotation)
-library(UpSetR)
-library(ComplexUpset)
-library(data.table)
+#library(data.table)
 #library(stringi)
 
 
@@ -40,7 +34,7 @@ type_data <- do.call("rbind", vcfs_survivor)
 names(type_data)[11]<-"path"
 
 type_data_good<-type_data%>% 
-    mutate(sample_info=sub('.*/\\s*', '', gsub("survivor_merged_filtered_PacBio_","",path)), ) %>% 
+    mutate(sample_info=sub('.*/\\s*', '', gsub("survivor_merged_filtered_PacBio_","",path)), ) %>%  
     mutate(sample_info=gsub(".vcf","",sample_info)) 
 
 
@@ -86,25 +80,26 @@ good_mat$sample_info<-gsub("overlapped_","",gsub("_matrix.txt","",good_mat$sampl
 good_mat_fin<-good_mat%>%
         rename(V1="CuteSV" , V2="nanoSV" , V3= "Sniffles",V4="Svim") %>% 
         mutate(caller_count=rowSums(.[1:4])) %>% 
-        mutate(sample_info=gsub("ways","caller",sample_info)) 
+        mutate(sample_info=gsub("ways","caller",sample_info)) %>% 
+        dplyr::select(-raw_id)
        
-good_mat_fin<-good_mat_fin[,colnames(good_mat_fin)!="raw_id"]
-       
-both<-cbind(type_data_inforow,good_mat_fin) %>% 
+
+
+both<-mutate(type_data_inforow,good_mat_fin) %>%     ### bind two dataframes and remove duplicated columns
       mutate(sample=gsub("_.*$","",sample_info)) %>% 
-      mutate(sample=gsub("1","GCT",sample)) %>% 
-      mutate(sample=gsub("2","RD",sample)) %>%
-      mutate(sample=gsub("3","SW",sample))
+      mutate(sample=gsub("1","GCT",
+                    gsub("2","RD",
+                    gsub("3","SW",sample))))
+
    
 
 chroms<-c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX")   
-both<-both[both$seqnames%in%chroms,]
-both<-both[both$CHR2%in%chroms,]
-both$caller_count_info<-sub('.*\\_', '', both$sample_info)
+both<-both[(both$seqnames%in%chroms) & (both$CHR2%in%chroms),]
+#both$caller_count_info<-sub('.*\\_', '', both$sample_info)
 
 
 samples<-unique(both$sample)
-callers<-c("1","2","3")
+callers<-c("1","2","3","4")
 var_types<-c(unique(both$SVTYPE))
 
 
@@ -122,8 +117,8 @@ for (kk in 1:length(samples)){  ### loop through each sample
     out_res<-NULL
     for(jj in 1:length(callers)){  ### loop through each number of variant caller detected a variant (caller_count_info column)
 
-        focal_caller<-focal[(focal$caller_count_info==paste(jj,"caller",sep ="") & focal$caller_count==jj),]  ### caller_count column
-
+        #focal_caller<-focal[(focal$caller_count_info==paste(jj,"caller",sep ="") & focal$caller_count==jj),]  ### caller_count column
+        focal_caller<-focal[focal$SUPP==callers[jj],]
         
         out_res_type<-NULL
         for (ii in 1:length(var_types)) { ### loop through each variant type
@@ -131,37 +126,46 @@ for (kk in 1:length(samples)){  ### loop through each sample
             focal_caller_vartype<-focal_caller[focal_caller$SVTYPE==var_types[ii],]
             if (var_types[ii]=="INV"){
         
-            focal_caller_vartype<-focal_caller_vartype[focal_caller_vartype$nanoSV!=1,]
+            focal_caller_vartype<-focal_caller_vartype[focal_caller_vartype$nanoSV!=1,] ### nanoSV inverions are not inversion
              }
 
-            if ((nrow(focal_caller_vartype)) >=57) {
+             #if (var_types[ii]=="INS"){ 
+             #
+            # focal_caller_vartype$END<-(focal_caller_vartype$END)
+             
+            #}
 
-            focal_random<-focal_caller_vartype[sample(nrow(focal_caller_vartype), 57+15,replace = TRUE) ,]
-
-             } 
+            #if ((nrow(focal_caller_vartype)) >=57) {
+            #
+            #focal_random<-focal_caller_vartype[sample(nrow(focal_caller_vartype), 57+15,replace = TRUE) ,]
+#
+             #} 
         
-           if ((nrow(focal_caller_vartype)) < 57) {
+           #if ((nrow(focal_caller_vartype)) < 57) {
 
-            focal_random<-focal_caller_vartype[sample(nrow(focal_caller_vartype),nrow(focal_caller_vartype), replace = TRUE) ,]
+            #focal_random<-focal_caller_vartype[sample(nrow(focal_caller_vartype),nrow(focal_caller_vartype), replace = TRUE) ,]
 
-             }    
+            # }    
 
+
+           focal_random<-focal_caller_vartype
+            
             out_res_type<-rbind(focal_random,out_res_type) 
             #out_res_type<-out_res_type[!duplicated(out_res_type$start_end),]
-        #out_res_type<-out_res_type[,c("seqnames","start", "end","width","strand", "paramRangeID","REF", "ALT","QUAL","FILTER" ,"sample_info", "CIEND", "CIPOS" , "CHR2" ,"END" ,"MAPQ" ,"RE", "IMPRECISE" ,"PRECISE" ,"SVLEN" ,"SVMETHOD","SVTYPE","SUPP_VEC","SUPP","STRANDS","CuteSV", "nanoSV" ,"Sniffles" ,"Svim" ,"sample_info.1", "caller_count" ,"sample" ,"start_end" ,"f" ,"r")]
+            #out_res_type<-out_res_type[,c("seqnames","start", "end","width","strand", "paramRangeID","REF", "ALT","QUAL","FILTER" ,"sample_info", "CIEND", "CIPOS" , "CHR2" ,"END" ,"MAPQ" ,"RE", "IMPRECISE" ,"PRECISE" ,"SVLEN" ,"SVMETHOD","SVTYPE","SUPP_VEC","SUPP","STRANDS","CuteSV", "nanoSV" ,"Sniffles" ,"Svim" ,"sample_info.1", "caller_count" ,"sample" ,"start_end" ,"f" ,"r")]
 
-        } ### variant type loop
+           } ### variant type loop
 
-        if(nrow(out_res_type)<284) {
+            #if(nrow(out_res_type)<284) {
 
-            additions<-focal_caller[focal_caller$SVTYPE=="DEL",]
-            additions_good<-additions[!(additions$start%in%out_res_type$start),]
+            #additions<-focal_caller[focal_caller$SVTYPE=="DEL",]
+            #additions_good<-additions[!(additions$start%in%out_res_type$start),]
             #additions_random<-additions_good[sample(nrow(additions_good), 284-nrow(out_res_type),replace = TRUE) ,]
-            additions_random<-additions_good[sample(nrow(additions_good), 330-nrow(out_res_type),replace = TRUE) ,]
+            #additions_random<-additions_good[sample(nrow(additions_good), 330-nrow(out_res_type),replace = TRUE) ,]
 
-            out_res_type<-rbind(out_res_type,additions_random)
+            #out_res_type<-rbind(out_res_type,additions_random)
 
-        } 
+        #} 
         
 
     out_res<-rbind(out_res_type,out_res)
@@ -169,7 +173,7 @@ for (kk in 1:length(samples)){  ### loop through each sample
     } ## number of callers loop
 
     #out_res[out_res$SVTYPE=="INS","END"]<-out_res[out_res$SVTYPE=="INS","start"]+out_res[out_res$SVTYPE=="INS","SVLEN"]
-    out_res_final<-out_res[,c("SVTYPE","seqnames","start","f","CHR2","END","r","ALT","CuteSV", "nanoSV", "Sniffles", "Svim" )] ## ALT column included for bracket information
+    out_res_final<-out_res[,c("SVTYPE","seqnames","start","f","CHR2","END","r","ALT","CuteSV", "nanoSV", "Sniffles", "Svim" )]
     out_res_final_dedup<-out_res_final %>% distinct()
 
     setwd("~/Dropbox/cancer_reserach/sarcoma/sarcoma_analysis/long_reads/pacbio1/sv_calling/survivor/panels")
@@ -179,6 +183,3 @@ for (kk in 1:length(samples)){  ### loop through each sample
 
 }  ## sample loop
     
-##############
-#### END #####
-##############
